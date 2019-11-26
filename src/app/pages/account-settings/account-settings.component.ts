45f6413d55a91618d6dac5cd7ac14662bf2ec651
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-11-26 17:24:03 +0700

    init account-settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit(form: NgForm) {
+    console.log(form);
+  }
 }
