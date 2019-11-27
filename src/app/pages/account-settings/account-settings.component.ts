9da08dd0c654c3aa055b9d5bc80c88c0cd0c7774
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2019-11-27 15:01:38 +0700

    update account-settings: add validators

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  NgForm,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,12 +14,41 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
+  formAccountSettings: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.formAccountSettings = new FormGroup(
+      {
+        name: new FormControl('', [Validators.required]),
+        username: new FormControl('', [Validators.required]),
+        lastPassword: new FormControl('', [Validators.required]),
+        newPassword: new FormControl(),
+        confirmPassword: new FormControl()
+      },
+      {
+        validators: (control: AbstractControl): ValidationErrors | null => {
+          const newPasswordVal = control.get('newPassword').value,
+            confirmPasswordVal = control.get('confirmPassword').value;
+          return (newPasswordVal || confirmPasswordVal) && (newPasswordVal !== confirmPasswordVal) ? { newPasswordNotMatch: true } : null;
+        }
+      }
+    );
+
+    const newPasswordCtrl = this.formAccountSettings.controls.newPassword,
+      confirmPasswordCtrl = this.formAccountSettings.controls.confirmPassword;
+    newPasswordCtrl.valueChanges.subscribe(value => {
+      if (value) {
+        confirmPasswordCtrl.setValidators([Validators.required]);
+      } else {
+        confirmPasswordCtrl.setValidators(null);
+      }
+      confirmPasswordCtrl.updateValueAndValidity();
+    });
   }
 
-  submit(form: NgForm) {
-    console.log(form);
+  submit() {
+    console.log(this.formAccountSettings);
   }
 }
